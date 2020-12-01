2ebb11dee4ca18191ed02df40925863fbbb3b861
mmf150 <1505715082@qq.com>
2020-12-01 11:44:53 +0800

    项目立项

---
+++ src/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName GitTest
+ * @Description 一句话描述此类或方法的作用
+ * @Author Java小菜鸡
+ * @Date 2020/12/1 11:29
+ * @Version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
